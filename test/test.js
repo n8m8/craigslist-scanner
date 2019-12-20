ca736ea3720864d991ec08764932587bb7ebdce6
Nathan Walls <nfw10@case.edu>
2019-12-19 20:52:06 -0700

    update package.json to include craigslist stuff, add a 'test test'

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var app = require('../index.js');
+var assert = require('assert');
+
+describe('my test', function() {
+  it('should equal', function() {
+    assert.equal(1, 1);
+  });
+});
